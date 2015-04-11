b9cc03fd5faf35370af378524b50987bbd832e5d
Behnam Ahmad Khan Beigi <yottanami@gnu.org>
2015-04-11 18:08:09 +0430

    white space removed

--- app/scripts/controllers/play.js
+++ app/scripts/controllers/play.js
@@ -24,15 +24,14 @@ angular.module('congkakApp')
         for(var i = 0; i < numbers.length; i++){
             for(var j=0; j < 8; j++){
                 $('.houses.player1 > .house.' + numbers[i]).prepend('<img class="ball" id="' + id + '" src="/images/ball.png">');
-                $('img#' + id).css({top: getRandomInt(-5,38), left: getRandomInt(-5,38)});
+                $('img#' + id).css({top: getRandomInt(-3,38), left: getRandomInt(-3,38)});
                 id = id + 1;
             }
             for(var j=0; j < 8; j++){
                 $('.houses.player2 > .house.' + numbers[i]).prepend('<img class="ball" id="' + id + '" src="/images/ball.png">');
-                $('img#' + id).css({top: getRandomInt(-5,38), left: getRandomInt(-5,38)});
+                $('img#' + id).css({top: getRandomInt(-3,38), left: getRandomInt(-3,38)});
                 id = id + 1;
             }
-
 
         }
       };
