75c10df8c8286b2b1dbcb99e2d5e922f5b2cf56a
Behnam Ahmad Khan Beigi <yottanami@gnu.org>
2015-04-12 17:35:35 +0430

    house directive structure created

--- app/scripts/controllers/play.js
+++ app/scripts/controllers/play.js
@@ -9,40 +9,10 @@
  */
 angular.module('congkakApp')
   .controller('PlayCtrl', function ($scope) {
-      var numbers = [
-          'one',
-          'two',
-          'three',
-          'four',
-          'five',
-          'six',
-          'seven'
-        ];
 
       $scope.initializeGame = function(){
-        var id = 0;
-        for(var i = 0; i < numbers.length; i++){
-            for(var j=0; j < 8; j++){
-                $('.houses.player1 > .house.' + numbers[i]).prepend('<img class="ball" id="' + id + '" src="/images/ball.png">');
-                $('img#' + id).css({top: getRandomInt(-3,38), left: getRandomInt(-3,38)});
-                id = id + 1;
-            }
-            for(var j=0; j < 8; j++){
-                $('.houses.player2 > .house.' + numbers[i]).prepend('<img class="ball" id="' + id + '" src="/images/ball.png">');
-                $('img#' + id).css({top: getRandomInt(-3,38), left: getRandomInt(-3,38)});
-                id = id + 1;
-            }
 
-        }
       };
-
-      $scope.distribute = function(div){
-          if ($scope.player1Turn){
-            console.log($('.houses.player2 > .house.' + numbers[div+1] + ' .ball').toArray());
-            var balls = $('.houses.player2 > .house.' + numbers[div+1] + ' .ball').toArray();
-          }
-      };
-
       $scope.initializeGame();
       $scope.player1Turn = true;
   });
--- app/scripts/directives/house.js
+++ app/scripts/directives/house.js
@@ -7,35 +7,48 @@
  * # house
  */
 angular.module('congkakApp')
-  .directive('house', function () {
-    return {
-        templateUrl: 'views/house.html',
-        restrict: 'E',
-        replace: 'true',
-        scope: {
-            id_number: '@idNumber'
-        },
-        link: function (scope, element, attrs) {
-            scope.numbers = [
-                'zero',
-                'one',
-                'two',
-                'three',
-                'four',
-                'five',
-                'six',
-                'seven'
-            ];
-            scope.random_position = getRandomInt(-3,38);
-            scope.capacity = 7;
-            scope.getRandomInt = function(min, max) {
-                min = min * 10;
-                max = max * 10;
-                return Math.floor(((Math.random() * (max - min + 1)) + min) / 10);
-            };
-            scope.getTimes = function(n){
-                return new Array(n);
-            };
-        }
-    };
-  });
+    .directive('house', function () {
+        return {
+            templateUrl: 'views/house.html',
+            restrict: 'E',
+            replace: 'true',
+            scope: {
+                id_number: '@idNumber'
+            },
+            link: function (scope, element, attrs) {
+                scope.numbers = [
+                    'zero',
+                    'one',
+                    'two',
+                    'three',
+                    'four',
+                    'five',
+                    'six',
+                    'seven',
+                    'eight',
+                    'nine',
+                    'ten',
+                    'eleven',
+                    'twelve',
+                    'thirteen',
+                    'fourteen'
+                ];
+                scope.random_position =30;
+                scope.capacity = 7;
+                /*
+                 scope.getRandomInt = function(min, max) {
+                 min = min * 10;
+                 max = max * 10;
+                 return Math.floor(((Math.random() * (max - min + 1)) + min) / 10);
+                 };
+                 */
+                scope.getTimes = function(n){
+                    return new Array(n);
+                };
+                scope.$on('distribute', function(event, args){
+                    console.log(args);
+                });
+
+            }
+        };
+    });
