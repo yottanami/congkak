9b584eeee42c20e0f73a080c0bb5d1df2a84d648
Behnam Ahmad Khan Beigi <yottanami@gnu.org>
2015-04-14 00:24:37 +0430

    debug codes removed

--- app/scripts/directives/house.js
+++ app/scripts/directives/house.js
@@ -48,44 +48,40 @@ angular.module('congkakApp')
                 };
                 scope.trigleClick = function(){
                     if (($rootScope.playerTurn == 1) && (scope.id_number < 15) && (scope.id_number > 7) && (scope.capacity > 0)){
-                        //console.log('out');
-                        //console.log(scope.id_number);
-                        //console.log(scope.capacity);
-                        //console.log('/out');
                         $rootScope.$broadcast('distribute', {idnumber: scope.id_number, hand: scope.capacity});
                         scope.capacity = 0;
-                    }else if($rootScope.playerTurn == 2 && args.idnumber < 8 && args.idnumber > 0 && args.capacity < 0){
+                    }else if($rootScope.playerTurn == 2 && scope.idnumber < 8 && scope.idnumber > 0 && scope.capacity > 0){
 
                     }
                 };
 
                 scope.trigleDistribute = function(hand){
-
                     $rootScope.$broadcast('distribute', {idnumber: scope.id_number, hand: hand});
                 };
 
                 $rootScope.$on('distribute', function(event, args){
-                    if (args.idnumber == 1){
-                        args.idnumber = 15;
+                    var hand = args.hand;
+                    var idnumber = args.idnumber;
+                    if (idnumber == 1){
+                        idnumber = 15;
                     }
 
-                    if ((args.idnumber == 8) && (scope.id_number == 8) && (args.hand > 0)){
+                    if ((idnumber == 8) && (scope.id_number == 8) && (hand > 0)){
                         $rootScope.$broadcast('houseDistribute', {user: 1});
-                        args.hand = args.hand - 1;
+                        hand = hand - 1;
                     }
 
-                    if ((scope.id_number == args.idnumber - 1)){
-                        if (args.hand > 1){
+                    if ((scope.id_number == idnumber - 1)){
+                        if (hand > 1){
                             scope.capacity = scope.capacity + 1;
-                            //console.log('current capacity' + scope.capacity);
-                            //console.log('id_number = ' + scope.id_number);
-                            //console.log('hand = ' + args.hand);
-
-                            scope.trigleDistribute(args.hand-1);
-                        }else if (args.hand == 1){
+                            hand = hand - 1;
+                            scope.trigleDistribute(hand);
+                        }else if (hand == 1){
                             if( scope.capacity > 0){
-                                $rootScope.$broadcast('distribute', {idnumber: scope.id_number, hand: scope.capacity + 1});
+                                scope.trigleDistribute(scope.capacity + hand);
                                 scope.capacity = 0;
+                            }else{
+                                console.log("------Player2--time----");
                             }
                         }
                     }
