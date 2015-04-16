30a5328c7d45341cc24a3a42a4adf5a949bc789e
Behnam Ahmad Khan Beigi <yottanami@gnu.org>
2015-04-16 23:00:51 +0430

    place of marbles modified

--- app/scripts/controllers/play.js
+++ app/scripts/controllers/play.js
@@ -10,10 +10,12 @@
 angular.module('congkakApp')
   .controller('PlayCtrl', function ($scope, $rootScope) {
 
-      $scope.move = function(item){
-          $("#animation_ball").css({top: 150, left: 420});
+      $scope.move = function(from, to){
+          $("#animation_ball").css({top: $("#"+from).css("top") , left: $("#"+from).css("left")});
           $("#animation_ball").show();
-          //$("#animation_ball").animate({left: "+=90", top: "+=0"}, 4000);
+          var toPositionTop = $("#" + to).css("top") - $("#" + from).css("top");
+          var toPositionLeft = $("#" + to).css("left") - $("#" + from).css("left");
+          $("#animation_ball").animate({left: "+="+ toPositionLeft , top: "+=" + toPositionT }, 4000);
           //$("#animation_ball").animate({left: "+=0", top: "+=80"}, 4000);
           //var left = "+40"
           //$("#animation_ball").animate({left: left, top: top}, 4000);
@@ -24,7 +26,6 @@ angular.module('congkakApp')
           $rootScope.boardStatus = [-1,7,7,7,7,7,7,7,7,7,7,7,7,7,7];
           $rootScope.gameLock = false;
           $('#animation_ball').hide();
-          $scope.move();
       };
       $scope.initializeGame();
 
